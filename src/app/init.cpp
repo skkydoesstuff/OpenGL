5796ba211c83f22ddd57463d84c16baa4100113e
skky <petesky26@gmail.com>
2026-04-27 04:37:37 -0400

    made window start out being maximised

--- src/app/init.cpp
+++ src/app/init.cpp
@@ -23,6 +23,7 @@ App::App() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+    glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
     glfwWindowHint(GLFW_SAMPLES, 4); // 4x MSAA
     
     // create window
